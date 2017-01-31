b4796e5ca54fed4113a7a64061afd2fcae965b33
David Mieloch <davidmieloch@gmail.com>
2017-01-31 14:45:31 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,9 @@
-def my_select(collection)
- # your code here!
+def my_select(nums)
+ # create new array to hold selection since we aren't using .collect or .map
+ my_selection = Array.new
+ # iterate through nums
+ # shovel nums to my_selection array if the yield method is true
+ nums.each { |number| my_selection << number if yield(number) }
+ # return new array since we are using .each and not .collect or .map
+ my_selection
 end
